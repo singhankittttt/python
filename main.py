55ba2a1407ae8a3af3600185d5ae5b1279059e95
singhankittttt <abhisingh912218@gmail.com>
2024-01-28 23:43:25 +0530

    update python file

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ print(name, age, price)
 print("name is singh")
 
 print("My name is singh")
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+''' keywords in python:
+and, else, in, return, as, except, is, True, assert, finally, lambda, try, break, false, nonlocal, with, for, None, while, from, not, yield, global, or, if, pass, import, raise
+'''
+
+num = 10
+num = num + 10
+num += 10
+num *= 10
+num /= 10
+print("num :", num)
+
