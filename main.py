44b00972d8ecd72ed3b39ab8b8df62d015463552
singhankittttt <abhisingh912218@gmail.com>
2024-01-29 15:02:29 +0530

    added some code in python

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello World")
+'''print("Hello World")
 print(20+85)
 
 name = "ankit"
@@ -13,16 +13,33 @@ print("My name is singh")
 
 print(type(name))
 print(type(age))
-print(type(price))
+print(type(price))'''
 
 ''' keywords in python:
 and, else, in, return, as, except, is, True, assert, finally, lambda, try, break, false, nonlocal, with, for, None, while, from, not, yield, global, or, if, pass, import, raise
 '''
 
-num = 10
+'''num = 10
 num = num + 10
 num += 10
 num *= 10
 num /= 10
-print("num :", num)
+print("num :", num)'''
 
+'''name = input("What is your name? ")
+print("Hello my name is " + name)
+
+old_age = input("enter your old age : ")
+new_age = int(old_age) + 2
+print(new_age)'''
+
+'''first = input("enter first number ")
+second = input("enter second number ")
+
+sum = int(first) + int(second)
+print("the sum is : " + str(sum))'''
+
+name = "Tony Stark"
+print(name.upper())
+# 0 index
+print(name.find('s'))
