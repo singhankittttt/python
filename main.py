437c99b241cd0545b8fad5c92f55dbedc6b7d589
singhankittttt <abhisingh912218@gmail.com>
2024-01-31 17:49:09 +0530

    added some code in python file

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ print(type(price))'''
 and, else, in, return, as, except, is, True, assert, finally, lambda, try, break, false, nonlocal, with, for, None, while, from, not, yield, global, or, if, pass, import, raise
 '''
 
+# Assignment Operator
 '''num = 10
 num = num + 10
 num += 10
@@ -39,10 +40,50 @@ second = input("enter second number ")
 sum = int(first) + int(second)
 print("the sum is : " + str(sum))'''
 
-name = "Tony Stark"
+'''name = "Tony Stark"
 print(name.upper())
 # 0 index
 print(name.find('S'))
 
 print("hi")
-print("my name is singh")
+print("my name is singh")'''
+
+'''a = 5
+b = 2
+print(a >= b)  # relational
+print(a ** b)  # power'''
+
+'''Arithmetic Operators(+, -, *, /, %, **)
+   Relational/Comparison Operators(==, !=, >, <, <=, <=)
+   Assignment Operators(=, +=, -=, *=, /=, %=, **=)
+   Logical Operator(not, and, or)'''
+
+# logical operator
+'''a = 50
+b = 30
+print(not False)
+print(not True)
+print(not (a > b))'''
+
+# AND Operator
+'''val1 = True
+val2 = False
+print("AND operator:", val1 and val2)'''
+
+# OR Operator
+'''val1 = True
+val2 = False
+# print("OR operator:", val1 or val2)
+print("OR operator", (a == b) or (a > b))'''
+
+# type conversion(automatically)
+'''a = int("2")
+b = 4.25
+
+# sum = a + b # 2.0 + 4.25 => 6.25
+# print(sum)
+
+# type casting
+print(type(a))
+print(a + b)'''
+
