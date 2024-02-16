5f2dd079d7e890758aeb7f47472d77e07772bfc5
singhankittttt <abhisingh912218@gmail.com>
2024-02-16 23:15:43 +0530

    added some code in python file

--- main.py
+++ main.py
@@ -475,3 +475,12 @@ while i < len(heroes):
 
 # 5.) search for a number x in this tuple using loop:
 # [1, 4, 9, 25, 36, 49, 64, 81, 100]
+
+nums = (1, 4, 9, 25, 36, 49, 64, 81, 100)
+
+x = 36
+
+i = 0
+while i < len(nums):
+    print(nums[i])
+    i += 1
