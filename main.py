8cfa0c3e2722f42874e7a28fec1dce6dc79dc7e5
singhankittttt <abhisingh912218@gmail.com>
2024-02-17 21:03:29 +0530

    added some code in python file

--- main.py
+++ main.py
@@ -573,10 +573,18 @@ for el in nums:
 for i in seq:
     print(i)'''
 
-for i in range(10):
+# for i in range(10):  # range(stop)
+#   print(i)
+
+# for i in range(2, 10):  # range(start, stop)
+#    print(i)
+
+for i in range(2, 10, 2):  # range(start, stop, step)
     print(i)
 
 # print(seq[0])
 # print(seq[1])
 # print(seq[2])
 # print(seq[3])
+
+# range(start?, stop, step?)
