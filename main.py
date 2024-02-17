713aa6986f8f67349a42aa29b21d26ea7c5e19c3
singhankittttt <abhisingh912218@gmail.com>
2024-02-17 18:00:04 +0530

    added some code in python file

--- main.py
+++ main.py
@@ -533,9 +533,29 @@ else:
 for num in tup:
     print(num)'''
 
-str = "anky"
+'''str = "ankypanky"
 
 for char in str:
+    if (char == 'y'):
+        print("y found")
+        break
     print(char)
 else:
-    print("end")
+    print("end")'''
+
+# using for loop
+# print the elements of the following list using a loop:
+# [1,4,9,16,25,36,49,64,81,100]
+
+'''list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+for num in list:
+    print(num)'''
+
+# search for a number x in this tuple using loop:
+# [1,4,9,16,25,36,49,64,81,100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+for el in nums:
+    
