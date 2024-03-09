cff62cbb956271024701fdc371804f7ae1ea4d01
singhankittttt <abhisingh912218@gmail.com>
2024-03-09 18:55:37 +0530

    py code

--- main.py
+++ main.py
@@ -1131,13 +1131,21 @@ class Account:
     def debit(self, amount):
         self.balance -= amount
         print("Rs.", amount, "was debited")
+        print("total balance = ", self.get_balance())
 
     # credit method
     def credit(self, amount):
         self.balance += amount
         print("Rs.", amount, "was credited")
+        print("total balance = ", self.get_balance())
+
+    def get_balance(self):
+        return self.balance
 
 
 acc1 = Account(10000, 123456)
-print(acc1.balance)
-print(acc1.account_no)
+acc1.debit(1000)
+acc1.credit(500)
+acc1.credit(40000)
+# print(acc1.balance)
+# print(acc1.account_no)
