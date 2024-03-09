397397b37f202199ef6c5043dd976e68a2922e50
singhankittttt <abhisingh912218@gmail.com>
2024-03-09 20:57:43 +0530

    py code

--- main.py
+++ main.py
@@ -1166,7 +1166,16 @@ print(s1.name)'''
 
 # private(like) attributes & methods
 # Conceptual implementations in python
-# 
+# private attributes & methods are meant to be used only within the class and are not accessible from outside the class.
+
 
 class Account:
-    def __init__(self, acc_no, acc_pass)
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no
+        self.acc_pass = acc_pass
+
+
+acc1 = Account("12345", "abcdef")
+
+print(acc1.acc_no)
+print(acc1.acc_pass)
