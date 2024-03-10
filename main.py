30eb6dea7666d1d826118e94dfb669ad417fb414
singhankittttt <abhisingh912218@gmail.com>
2024-03-10 20:44:17 +0530

    py code

--- main.py
+++ main.py
@@ -1172,10 +1172,14 @@ print(s1.name)'''
 class Account:
     def __init__(self, acc_no, acc_pass):
         self.acc_no = acc_no
-        self.acc_pass = acc_pass
+        self.__acc_pass = acc_pass
+
+    def reset_pass(self):
+        print(self.__acc_pass)
 
 
 acc1 = Account("12345", "abcdef")
 
 print(acc1.acc_no)
-print(acc1.acc_pass)
+print(acc1.reset_pass())
+# print(acc1.__acc_pass)
